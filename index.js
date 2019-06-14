264f92ff09ad5d4ffe4c954dfa99590f4265f55b
Nelly Jordan ordoñez <njordancastillo@hotmail.com>
2019-06-14 09:15:07 -0500

    prueba json2

--- index.js
+++ index.js
@@ -14,9 +14,13 @@ app.get('/api',(req,res)=>{
     res.send("info api")
 })
 
+app.get('/api/pepitoPeres',(req,res)=>{
+    res.send({test:"1231"})
+})
+
 //desde aqui Nelly
 app.get('api/articulos',(req,res)=>{
-    res.send({articulo1:"prueba"})
+    res.send({articulos:"prueba"})
 })
 
 server=http.createServer(app);
