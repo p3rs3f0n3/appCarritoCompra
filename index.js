b34ca299acd0fd3d96c2fb71cc6fd125483fd64a
Nelly Jordan ordoñez <njordancastillo@hotmail.com>
2019-06-14 08:59:06 -0500

    prueba json

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ var http= require('http')
 var port = 3000
 
 app=express()
+
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static('client'))
 app.use(bodyParser.json())
+
 app.get('/api',(req,res)=>{
     res.send("info api")
 })
 
 app.get('/api/pepitoPeres',(req,res)=>{
     res.send({test:"1231"})
+})
+
+//desde aqui Nelly
+app.get('api/articulos',(req,res)=>{
+    res.send({articulo1:"prueba"})
 })
 
 server=http.createServer(app);
